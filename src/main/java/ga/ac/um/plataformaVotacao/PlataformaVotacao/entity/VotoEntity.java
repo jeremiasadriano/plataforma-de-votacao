406bf1b474dd4e1bd14df4845d3bffe93d0a0593
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2023-10-26 14:00:25 -0400

    Consertar o cocô que fiz,

--- src/main/java/ga/ac/um/plataformaVotacao/PlataformaVotacao/entity/VotoEntity.java
+++ src/main/java/ga/ac/um/plataformaVotacao/PlataformaVotacao/entity/VotoEntity.java
@@ -29,7 +29,7 @@ public class VotoEntity {
     private Long estudanteId;
 
     @NotNull
-    @OneToMany(cascade = CascadeType.REMOVE)
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "voto_id", referencedColumnName = "id")
     private Set<OpcoesVotos> opcoesVotos = new HashSet<>();
 }
